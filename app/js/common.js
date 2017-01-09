cca4dd0cf108089d1cde27e4759e24e4cb5894c6
Boris Sizov <Boris Sizov>
2017-01-09 21:35:59 +0300

    init

---
+++ app/js/common.js
@@ -0,0 +1,75 @@
+$('document').ready(function() {
+
+	// Owl carousel plugin https://owlcarousel2.github.io/OwlCarousel2/docs/started-welcome.html
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+	});
+  
+  // Owl carousel plugin https://owlcarousel2.github.io/OwlCarousel2/docs/started-welcome.html
+  $('.owl-carousel-2').owlCarousel({
+    loop:true,
+    margin:10,
+    items:3,
+    nav:false
+    
+	});
+  
+    // Owl carousel plugin https://owlcarousel2.github.io/OwlCarousel2/docs/started-welcome.html
+  $('.owl-carousel-3').owlCarousel({
+    loop:true,
+    margin:28,
+    items:3,
+    nav:false
+    
+	});
+    $('.owl-carousel-4').owlCarousel({
+    loop:true,
+    margin:28,
+    items:4,
+    nav:false
+    
+    });
+    $('.owl-carousel-5').owlCarousel({
+    nav: true,
+    loop: true,
+    margin: 28,
+    items: 1,
+    navText: ["&#32;","&#32;"]
+    
+    });
+  //ion slider docs: http://ionden.com/a/plugins/ion.rangeSlider/en.html
+    $("#range_03").ionRangeSlider({
+        type: "double",
+        // grid: true,
+        min: 5,
+        max: 200,
+        from: 10,
+        to: 90,
+        hide_min_max: true,
+        postfix: " &euro;"
+    });
+        $("#range_04").ionRangeSlider({
+        type: "double",
+        // grid: true,
+        min: 5,
+        max: 200,
+        from: 10,
+        to: 90,
+        hide_min_max: true,
+        postfix: " &euro;"
+    });
+
+
+});
---
+++ readme.md
@@ -0,0 +1,8 @@
+<h1>Demo project of commercial website</h1>
+
+
+>	PSD files taken from real job offer at upwork.com
+
+<p>
+<small>all copyrights belong to their respective owners</small>
+</p>
